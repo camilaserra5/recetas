4a607918017663f75f77549d33744bb7c7c7992f
= <camilaserra5@gmail.com>
2022-08-22 21:44:35 -0300

    wip

--- src/index.js
+++ src/index.js
@@ -1,9 +1,13 @@
-import { ColorModeScript } from '@chakra-ui/react';
+import {ColorModeScript, theme} from '@chakra-ui/react';
 import React, { StrictMode } from 'react';
 import * as ReactDOM from 'react-dom/client';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import * as serviceWorker from './serviceWorker';
+import {
+    ChakraProvider,
+} from '@chakra-ui/react';
+import customTheme from "./utils/theme";
 
 const container = document.getElementById('root');
 const root = ReactDOM.createRoot(container);
@@ -11,7 +15,9 @@ const root = ReactDOM.createRoot(container);
 root.render(
   <StrictMode>
     <ColorModeScript />
-    <App />
+      <ChakraProvider theme={customTheme}>
+          <App />
+      </ChakraProvider>
   </StrictMode>
 );
 
---
+++ src/recetas.js
@@ -0,0 +1,45 @@
+export const recipeData = [
+    {
+        name: "Brownie",
+        servings: 2,
+        prepTime: 10,
+        cookTime: 20,
+        totalTime: 30,
+        ingredients: [
+            {
+                name:"chocolate",
+                quantity:2,
+                unit:"barras"
+            },
+            {
+                name:"huevo",
+                quantity:4,
+                unit:"unidades"
+            }
+        ],
+        directions: [
+            "primero asdjhkasjhd ashdkasjh kdha",
+            "",
+            ""
+        ],
+    },
+    {
+        name: "Brownie parte 2",
+        servings: 2,
+        prepTime: 30,
+        cookTime: 20,
+        totalTime: 30,
+        ingredients: [
+            {
+                name:"caca",
+                quantity:3,
+                unit:"barras"
+            },
+            {
+                name:"huevo",
+                quantity:4,
+                unit:"unidades"
+            }
+        ],
+    },
+];
